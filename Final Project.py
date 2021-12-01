c4b71b073425005c7392c83a1f63e85ca152f7e6
preston798 <95325859+preston798@users.noreply.github.com>
2021-11-30 20:59:13 -0500

    Add files via upload

---
+++ Final Project.py
@@ -0,0 +1,38 @@
+# Final Project
+# Preston Hancock
+# This will be a wheel of fortune game. The program will select a category that has a list attached to it with the correct word. The program will create a new list that shows only a couple of the characters and the rest blank. 
+# The user will guess a constonant. If it is in the hidden list, they will appear on the actual list and money will be added to the user's account. The user can also buy a vowel. There will be another box where they can guess the entire word or phrase.
+# The user will "spin" a wheel in the background to get money added to their account if they choose a letter correctly. 
+# 
+# The program will first initilize the GUI and select a a category from the dictionary. The dictionary will be stored in a .txt file.   
+
+from breezypythongui import EastFrame, EasyFrame
+
+class WheelOfF(EasyFrame):
+    def __init__(self):
+        EasyFrame.__init__(self)
+
+
+    def chooseConstonant():
+        return
+    
+    def buyVowel():
+        return
+
+    def spinWheel():
+        return
+    
+    def guessPhrase():
+        return
+    
+def importFile(filename):
+    return
+
+
+
+def main():
+    WheelOfF().mainloop()
+
+if __name__ == "__main__":
+    main()
+
